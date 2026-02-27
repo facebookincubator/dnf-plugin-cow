fc8c606b2271b133f32a006e07f84d64858cb425
Matteo Croce <teknoraver@meta.com>
2026-02-27 20:36:37 +0100

    don't even transcode packages if filesystem doesn't support reflinks
    Currently only XFS and BtrFS support it.

--- reflink.py
+++ reflink.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 #
+import ctypes
+import ctypes.util
 import os
 
 import dnf
@@ -12,6 +14,39 @@ TRANSCODER_PATHS = [
     "/usr/lib/rpm/rpm2extents",
     "/usr/bin/rpm2extents",
 ]
+
+# Filesystem magic numbers for filesystems supporting reflinks
+XFS_SUPER_MAGIC = 0x58465342
+BTRFS_SUPER_MAGIC = 0x9123683E
+
+REFLINK_FILESYSTEMS = {XFS_SUPER_MAGIC, BTRFS_SUPER_MAGIC}
+
+
+class _statfs(ctypes.Structure):
+    _fields_ = [
+        ("f_type", ctypes.c_long),
+        ("f_bsize", ctypes.c_long),
+        ("f_blocks", ctypes.c_ulong),
+        ("f_bfree", ctypes.c_ulong),
+        ("f_bavail", ctypes.c_ulong),
+        ("f_files", ctypes.c_ulong),
+        ("f_ffree", ctypes.c_ulong),
+        ("f_fsid", ctypes.c_long * 2),
+        ("f_namelen", ctypes.c_long),
+        ("f_frsize", ctypes.c_long),
+        ("f_flags", ctypes.c_long),
+        ("f_spare", ctypes.c_long * 4),
+    ]
+
+
+_libc = ctypes.CDLL(ctypes.util.find_library("c"), use_errno=True)
+
+
+def _get_fs_type(path):
+    buf = _statfs()
+    if _libc.statfs(path.encode(), ctypes.byref(buf)) != 0:
+        return 0
+    return buf.f_type
 
 
 def find_transcoder():
@@ -32,6 +67,11 @@ class DnfReflink(dnf.Plugin):
         # command that starts with "Download" wants the un-transcoded package
         if self.cli.command.__class__.__name__.lower().startswith("download"):
             return
+
+        # Skip transcoding if the cache filesystem doesn't support reflinks
+        if _get_fs_type(self.base.conf.cachedir) not in REFLINK_FILESYSTEMS:
+            return
+
         transcoder = find_transcoder()
         if transcoder:
             os.environ["LIBREPO_TRANSCODE_RPMS"] = f"{transcoder} SHA256"
